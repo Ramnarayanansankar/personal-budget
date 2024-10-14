abd70a88fcaa27f4946d425458ef6925ab1ee85e
Ramnarayamsankar <ramnarayamsankar@gmail.com>
2024-10-13 22:22:15 -0400

    Made changes for assignment-8

--- server.js
+++ server.js
@@ -1,23 +1,61 @@
-const cors = require('cors');
-const port = 3000;
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const express = require("express");
 const app = express();
+const port = 3000;
+const cors = require("cors");
+app.use("/", express.static("public"));
+app.use(express.json());
 
+const mongoose = require("mongoose");
+const budgetModel = require("./models/budget_schema");
+
+let url = "mongodb+srv://cluster0.do7jx.mongodb.net";
 app.use(cors());
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use('/',express.static('public'));
+app.get("/budget", (req, res) => {
+  mongoose
+    .connect(url)
+    .then(() => {
+      console.log("Connected to database");
+      // Fetch
+      budgetModel
+        .find({})
+        .then((data) => {
+          res.send(data);
+          mongoose.connection.close();
+        })
+        .catch((connectionError) => {
+          console.log(connectionError);
+        });
+    })
+    .catch((connectionError) => {
+      console.log(connectionError);
+    });
+});
 
-app.get('/myBudget', (req, res) => { 
-    let rawdata = fs.readFileSync('myBudget.json');
-    let myBudget = JSON.parse(rawdata);
-    res.json(myBudget);
+app.post("/addBudgetData", (req, res) => {
+  mongoose
+    .connect(url)
+    .then(() => {
+      // Insert
+      let newData = new budgetModel(req.body);
+      budgetModel
+        .insertMany(newData)
+        .then((data) => {
+          res.send("Data Entered Successfully");
+          mongoose.connection.close();
+        })
+        .catch((connectionError) => {
+          res.send(connectionError.message);
+        });
+    })
+    .catch((connectionError) => {
+      res.send(connectionError);
+    });
 });
 
 app.listen(port, () => {
-    console.log(`App Served at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
-
